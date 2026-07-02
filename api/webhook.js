890dccb5e2be7f8696a2204171f9d24d507f55ff
bdsadar <sadaruddin431@gmail.com>
2026-07-02 20:32:59 +0600

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -42,7 +42,14 @@ export default async function handler(req, res) {
       return res.status(200).send(challenge);
     }
     return res.status(403).send("Forbidden");
+    console.log({
+  mode: req.query["hub.mode"],
+  token: req.query["hub.verify_token"],
+  challenge: req.query["hub.challenge"],
+  expected: process.env.VERIFY_TOKEN,
+});
   }
+ 
 
   // POST — Incoming message event
   if (req.method === "POST") {
